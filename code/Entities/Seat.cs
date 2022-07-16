a1704d7a6b0f5c84b4c949adf3fce6b6f662c003
Alex <alex@gu3.me>
2022-07-16 21:22:32 +0100

    Add seat entity

---
+++ code/Entities/Seat.cs
@@ -0,0 +1,17 @@
+﻿using Sandbox;
+using SandboxEditor;
+
+namespace Poker;
+
+[EditorModel( "models/editor/playerstart.vmdl" )]
+[Library( "info_seat" )]
+[Title( "Seat" ), Category( "Poker" )]
+[HammerEntity]
+public class Seat : Entity
+{
+	[Property]
+	public int SeatNumber { get; set; }
+
+	[Property]
+	public string SeatName { get; set; }
+}
