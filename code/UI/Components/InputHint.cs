8a2a1dc9f3607dbcfc286720c93c1d5a5d86afc0
Alex <alex@gu3.me>
2022-07-16 14:21:15 +0100

    Basic input layer

--- code/UI/Components/InputHint.cs
+++ code/UI/Components/InputHint.cs
@@ -1,66 +1,92 @@
 using Sandbox;
 using Sandbox.UI;
-using System;
 
 namespace Poker.UI;
 
 [UseTemplate]
 public partial class InputHint : Panel
 {
-    // @ref
-    public Image Glyph { get; set; }
-    public InputButton Button { get; set; }
-    public string Text { get; set; }
-    public Label ActionLabel { get; set; }
+	// @ref
+	public Image Glyph { get; set; }
+	public string Name { get; set; }
+	public string Text { get; set; }
+	public Label ActionLabel { get; set; }
 
-    protected bool IsSet = false;
+	protected bool IsSet = false;
 
-    public InputHint()
-    {
-        BindClass( "noaction", () => string.IsNullOrEmpty( Text ) );
-    }
+	public InputHint()
+	{
+		BindClass( "noaction", () => string.IsNullOrEmpty( Text ) );
+	}
 
-    public override void SetProperty( string name, string value )
-    {
-        base.SetProperty( name, value );
+	public override void SetProperty( string name, string value )
+	{
+		base.SetProperty( name, value );
 
-        if ( name == "btn" )
-        {
-            SetButton( Enum.Parse<InputButton>( value, true ) );
-        }
-    }
+		Log.Trace( (name, value) );
+		if ( name == "btn" )
+		{
+			SetButton( value );
+		}
+	}
 
-    public void SetButton( InputButton button )
-    {
-        Button = button;
-        IsSet = true;
-    }
+	public void SetButton( string name )
+	{
+		Name = name;
+	}
 
-    public override void SetContent( string value )
-    {
-        base.SetContent( value );
+	public override void SetContent( string value )
+	{
+		base.SetContent( value );
 
-        ActionLabel.SetText( value );
-        Text = value;
-    }
+		ActionLabel.SetText( value );
+		Text = value;
+	}
 
-    public override void Tick()
-    {
-        base.Tick();
+	protected override void PostTemplateApplied()
+	{
+		base.PostTemplateApplied();
+	}
 
-        if ( IsSet )
-        {
-            Texture glyphTexture = Input.GetGlyph( Button, InputGlyphSize.Small, GlyphStyle.Knockout.WithSolidABXY().WithNeutralColorABXY() );
-            if ( glyphTexture != null )
-            {
-                Glyph.Style.BackgroundImage = glyphTexture;
-                Glyph.Style.Width = glyphTexture.Width;
-                Glyph.Style.Height = glyphTexture.Height;
-            }
-            else
-            {
-                Glyph.Style.BackgroundImage = Texture.Load( FileSystem.Mounted, "/ui/Input/invalid_glyph.png" );
-            }
-        }
-    }
+	public override void Tick()
+	{
+		base.Tick();
+
+		// if ( !IsSet )
+		{
+			InputButton button = InputButton.Slot0;
+
+			var action = InputLayer.GetAction( Name );
+
+			if ( action is Action1D action1D )
+			{
+				button = action1D.PositiveButton;
+			}
+			else if ( action is ActionBool actionBool )
+			{
+				button = actionBool.Button;
+			}
+			else if ( action is ActionAxis actionAxis )
+			{
+				button = actionAxis.DisplayButton;
+			}
+
+			SetClass( "active", !action.Evaluate().AlmostEqual( 0.0f, 0.01f ) );
+
+			Texture glyphTexture = Input.GetGlyph( button, InputGlyphSize.Small, GlyphStyle.Knockout.WithSolidABXY().WithNeutralColorABXY() );
+
+			if ( glyphTexture != null )
+			{
+				Glyph.Style.BackgroundImage = glyphTexture;
+				Glyph.Style.Width = glyphTexture.Width;
+				Glyph.Style.Height = glyphTexture.Height;
+			}
+			else
+			{
+				Glyph.Style.BackgroundImage = Texture.Load( FileSystem.Mounted, "/ui/Input/invalid_glyph.png" );
+			}
+
+			IsSet = true;
+		}
+	}
 }
---
+++ code/Utils/InputLayer.cs
@@ -0,0 +1,144 @@
+﻿using Sandbox;
+using SandboxEditor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Poker;
+
+public class InputAction
+{
+	public string Name { get; set; }
+
+	public InputAction( string name )
+	{
+		Name = name;
+	}
+
+	public virtual float Evaluate()
+	{
+		return 0.0f;
+	}
+}
+
+public class ActionBool : InputAction
+{
+	public InputButton Button { get; set; }
+
+	public ActionBool( string name, InputButton button ) : base( name )
+	{
+		Button = button;
+	}
+
+	public override string ToString()
+	{
+		return $"Boolean {Name}: {Button} ({Evaluate()})";
+	}
+	public override float Evaluate()
+	{
+		return Input.Down( Button ) ? 1.0f : 0.0f;
+	}
+}
+
+public class Action1D : InputAction
+{
+	public InputButton PositiveButton { get; set; }
+	public InputButton NegativeButton { get; set; }
+
+	public Action1D( string name, InputButton positiveButton, InputButton negativeButton ) : base( name )
+	{
+		PositiveButton = positiveButton;
+		NegativeButton = negativeButton;
+	}
+
+	public override string ToString()
+	{
+		return $"1D {Name}: {PositiveButton}, {NegativeButton} ({Evaluate()})";
+	}
+
+	public override float Evaluate()
+	{
+		if ( Input.Down( PositiveButton ) )
+			return 1.0f;
+
+		if ( Input.Down( NegativeButton ) )
+			return -1.0f;
+
+		return 0.0f;
+	}
+}
+
+public class ActionAxis : InputAction
+{
+	public Func<float> Function { get; set; }
+	public InputButton DisplayButton { get; set; }
+
+	public ActionAxis( string name, Func<float> function, InputButton displayButton ) : base( name )
+	{
+		Function = function;
+		DisplayButton = displayButton;
+	}
+
+	public override string ToString()
+	{
+		return $"Axis {Name}: {DisplayButton} ({Evaluate()})";
+	}
+
+	public override float Evaluate()
+	{
+		return Function?.Invoke() ?? 0.0f;
+	}
+}
+
+public class InputLayer
+{
+	public static List<InputAction> ControllerActions => new()
+	{
+		new ActionBool( "fold", InputButton.Use ),
+		new ActionAxis( "adjust_amount", () => Input.Forward, InputButton.Run ),
+		new ActionBool( "submit", InputButton.Jump ),
+		new ActionBool( "your_cards", InputButton.SecondaryAttack ),
+		new ActionBool( "community_cards", InputButton.PrimaryAttack ),
+	};
+
+	public static List<InputAction> PCActions => new()
+	{
+		new ActionBool( "fold", InputButton.Flashlight ),
+		new Action1D( "adjust_amount", InputButton.Forward, InputButton.Back ),
+		new ActionBool( "submit", InputButton.Jump ),
+		new ActionBool( "your_cards", InputButton.Duck ),
+		new ActionBool( "community_cards", InputButton.Run ),
+	};
+
+	public static List<InputAction> Actions
+	{
+		get
+		{
+			if ( Input.UsingController )
+				return ControllerActions;
+
+			return PCActions;
+		}
+	}
+
+	public static InputAction GetAction( string name )
+	{
+		return Actions.FirstOrDefault( x => x.Name == name );
+	}
+
+	public static float Evaluate( string name )
+	{
+		return GetAction( name ).Evaluate();
+	}
+
+	[DebugOverlay( "inputlayer", "Input Layer", "sports_esports" )]
+	public static void OnDrawHud()
+	{
+		if ( !Host.IsClient )
+			return;
+
+		OverlayUtils.BoxWithText( Render.Draw2D, new Vector2( 45, 175 ),
+			"Input Layer",
+			$"{string.Join( "\n", Actions )}" );
+	}
+}
