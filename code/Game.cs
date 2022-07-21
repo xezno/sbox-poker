c34ed17f969ea07601ab3ac619ba9dba35e05c82
Alex <alex@gu3.me>
2022-07-21 23:12:44 +0100

    Rename ExampleHud* to Hud (should've done this a lot sooner)

--- code/Game.cs
+++ code/Game.cs
@@ -6,58 +6,58 @@ namespace Poker;
 
 public partial class Game : Sandbox.Game
 {
-	public PokerControllerEntity PokerControllerEntity { get; set; }
+    public PokerControllerEntity PokerControllerEntity { get; set; }
 
-	public Game()
-	{
-		if ( IsServer )
-		{
-			_ = new ExampleHudEntity();
-		}
-	}
+    public Game()
+    {
+        if ( IsServer )
+        {
+            _ = new Hud();
+        }
+    }
 
-	[ConCmd.Server( "poker_start" )]
-	public static void StartPokerGame()
-	{
-		var instance = Game.Current as Game;
+    [ConCmd.Server( "poker_start" )]
+    public static void StartPokerGame()
+    {
+        var instance = Game.Current as Game;
 
-		instance.PokerControllerEntity?.Delete();
-		instance.PokerControllerEntity = new();
-		instance.PokerControllerEntity.Run();
-	}
+        instance.PokerControllerEntity?.Delete();
+        instance.PokerControllerEntity = new();
+        instance.PokerControllerEntity.Run();
+    }
 
-	public override void ClientJoined( Client client )
-	{
-		base.ClientJoined( client );
+    public override void ClientJoined( Client client )
+    {
+        base.ClientJoined( client );
 
-		var player = new Player();
-		var clothingContainer = new ClothingContainer();
+        var player = new Player();
+        var clothingContainer = new ClothingContainer();
 
-		if ( client.IsBot )
-			clothingContainer.LoadRandom();
-		else
-			clothingContainer.LoadFromClient( client );
+        if ( client.IsBot )
+            clothingContainer.LoadRandom();
+        else
+            clothingContainer.LoadFromClient( client );
 
-		player.AvatarData = clothingContainer.Serialize();
-		clothingContainer.DressEntity( player );
+        player.AvatarData = clothingContainer.Serialize();
+        clothingContainer.DressEntity( player );
 
-		MoveToSpawnpoint( player );
-		client.Pawn = player;
-	}
+        MoveToSpawnpoint( player );
+        client.Pawn = player;
+    }
 
-	public override void MoveToSpawnpoint( Entity pawn )
-	{
-		var clientIndex = Client.All.Count - 1;
-		var spawnpoint = Entity.All.OfType<Seat>().ToList().First( x => x.SeatNumber == clientIndex );
+    public override void MoveToSpawnpoint( Entity pawn )
+    {
+        var clientIndex = Client.All.Count - 1;
+        var spawnpoint = Entity.All.OfType<Seat>().ToList().First( x => x.SeatNumber == clientIndex );
 
-		Log.Trace( $"Finding spawnpoint for client {clientIndex}" );
+        Log.Trace( $"Finding spawnpoint for client {clientIndex}" );
 
-		if ( spawnpoint == null )
-		{
-			Log.Warning( $"Couldn't find spawnpoint for {pawn}!" );
-			return;
-		}
+        if ( spawnpoint == null )
+        {
+            Log.Warning( $"Couldn't find spawnpoint for {pawn}!" );
+            return;
+        }
 
-		pawn.Transform = spawnpoint.Transform;
-	}
+        pawn.Transform = spawnpoint.Transform;
+    }
 }
---
+++ code/UI/Hud.cs
@@ -0,0 +1,15 @@
+﻿using Sandbox;
+using Sandbox.UI;
+
+namespace Poker;
+
+public class Hud : HudEntity<RootPanel>
+{
+	public Hud()
+	{
+		if ( IsClient )
+		{
+			RootPanel.SetTemplate( "/Code/UI/Hud.html" );
+		}
+	}
+}
