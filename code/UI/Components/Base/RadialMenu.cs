24f8cfa176300c5a79f5d7714556ad4b26b9fb3b
Alex <alex@gu3.me>
2022-11-30 02:44:27 +0000

    Start working on circular- / radial-based betting UI

---
+++ code/UI/Components/Base/RadialMenu.cs
@@ -0,0 +1,54 @@
+﻿using Sandbox.UI;
+using System;
+
+namespace Poker.UI;
+
+public class RadialMenu : Panel
+{
+	private Panel Hovered { get; set; }
+
+	public string HoverText { get; set; } = "";
+	public bool IsActive { get; set; }
+
+	[Alias( "radialmenu" )]
+	public override void Tick()
+	{
+		base.Tick();
+
+		var children = Children.ToList();
+
+		float angleIncrement = 360f / children.Count;
+		var distance = 48f * ScaleToScreen;
+		var angleOffset = (children.Count % 2 == 0) ? 0f : 30f;
+
+		for ( int i = 0; i < children.Count; i++ )
+		{
+			var child = children[i];
+
+			var angle = i * angleIncrement;
+			angle -= angleOffset;
+
+			var position = new Vector2(
+				MathF.Cos( angle.DegreeToRadian() ) * distance,
+				MathF.Sin( angle.DegreeToRadian() ) * distance
+			);
+
+			position += Box.Rect.Width / 2f;
+
+			child.Style.Left = position.x * ScaleFromScreen;
+			child.Style.Top = position.y * ScaleFromScreen;
+		}
+
+		Hovered = children.FirstOrDefault( x => x.HasHovered );
+
+		if ( HasHovered && Hovered != null && Hovered is RadialMenuItem item )
+		{
+			HoverText = item.Name;
+			IsActive = true;
+		}
+		else
+		{
+			IsActive = false;
+		}
+	}
+}
---
+++ code/UI/Components/Base/RadialMenuItem.cs
@@ -0,0 +1,42 @@
+﻿using Sandbox.UI;
+using Sandbox.UI.Construct;
+using System;
+
+namespace Poker.UI;
+
+[Alias( "radialmenuitem" )]
+public class RadialMenuItem : Panel
+{
+	private Label IconLabel { get; set; }
+	private Panel ActionPanel { get; set; }
+
+	public Action Callback { get; set; }
+
+	public string Name { get; set; }
+
+	public RadialMenuItem()
+	{
+		AddClass( "icon" );
+
+		IconLabel = Add.Label( "", "icon" );
+		ActionPanel = Add.Panel( "action" );
+	}
+
+	public override void SetProperty( string name, string value )
+	{
+		base.SetProperty( name, value );
+
+		if ( name == "icon" )
+		{
+			IconLabel.Text = value;
+		}
+		else if ( name == "action" )
+		{
+			Log.Trace( $"TODO: set action to {value}" );
+		}
+		else if ( name == "name" )
+		{
+			Name = value;
+		}
+	}
+}
